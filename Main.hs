e1a98b4ad03d11bef57031902a399fefec746a30
Sasa Bogicevic <bogicevicsasa@yahoo.com>
2017-08-17 12:03:21 +0200

    Add top level docs
    Change types to [Char] for now

--- Main.hs
+++ Main.hs
@@ -1,9 +1,26 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Web.Scotty
+import Data.Text
+
+
+
+-- | api url for data fetching
+api_url :: Text
+api_url = "https://query1.finance.yahoo.com/v7/finance/download/%s?period1=%s&period2=%s&interval=%s&events=history&crumb=%s"
+
+-- | url for the api login
+login_url :: Text
+login_url = "https://finance.yahoo.com/quote/"
+
+-- | Build the url for the api call
+buildApiUrl :: [Char] -> [Char] -> [Char] -> [Char] -> [Char] -> [Char]
+buildApiUrl download period1 period2 interval crumb =
+    "https://query1.finance.yahoo.com/v7/finance/download/" ++ download ++
+    "?period1=" ++ period1 ++ "&period2=" ++ period2 ++ "&interval=" ++
+     interval ++ "&events=history&crumb=" ++ crumb
 
 main :: IO ()
 main = scotty 3000 $ do
-  get "/:word" $ do
-    beam <- param "word"
-    html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
+  get "/" $ do
+    html $ mconcat ["<h1>Yahoo historical data downloader</h1>"]
