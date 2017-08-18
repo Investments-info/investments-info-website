9923b8b5df2667f58d97d53fe8fdbc36dfc8376a
Sasa Bogicevic <bogicevicsasa@yahoo.com>
2017-08-18 18:00:51 +0200

    Add types to save data to and json conversion

--- Main.hs
+++ Main.hs
@@ -1,15 +1,43 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
 
 import Network.Wreq
 
 import Control.Lens
-import Data.Aeson (toJSON)
+import Data.Aeson (ToJSON, FromJSON)
 
 -- Easy traversal of JSON data.
 import Data.Aeson.Lens (key, nth)
 import Data.Text
+import Data.Map as Map
+import Data.Aeson (Value)
+import Control.Monad.IO.Class
+import GHC.Generics (Generic)
+import qualified Control.Exception as E
 
 
+type Resp = Response (Map String Value)
+
+data Fdata = Fdata {
+    fdataSymbol :: Maybe String
+    ,fdataTimestampl :: Maybe String
+    ,fdataTradingDay :: Maybe String
+    ,fdataOpen :: Maybe Double
+    ,fdataHigh :: Maybe Double
+    ,fdataLow :: Maybe Double
+    ,fdataClose :: Maybe Double
+    ,fdataVolume :: Maybe Integer
+    ,fdataOpenInterest :: Maybe String
+   } deriving (Show, Generic)
+
+instance ToJSON Fdata
+instance FromJSON Fdata
+
+
+-- | Barchart api
+api_key = "b47d9c51c11fee9020293a37f9b65557"
+
+history = "http://marketdata.websol.barchart.com/getHistory.json?apikey=" ++ api_key ++ "&symbol=IBM&type=daily&startDate=20160818000000"
 
 -- | api url for data fetching
 api_url :: Text
@@ -28,8 +56,13 @@ buildApiUrl download period1 period2 interval crumb =
 
 main :: IO ()
 main = do
-    putStrLn "Yahoo historical data downloader"
-    putStrLn "get http://httpbin.org/get"
-    r <- get "http://httpbin.org/get"
-    putStrLn "response status : "
-    print (r ^. responseStatus . statusCode)
+  putStrLn "Yahoo historical data downloader"
+  r <- get history
+  -- This first conversion attempt will fail, but because we're using
+  -- Either, it will not throw an exception that kills execution.
+  let failing = asJSON r :: Either E.SomeException (Response [Int])
+  print failing
+
+  -- Our second conversion attempt will succeed.
+  let succeeding = asJSON r :: Either E.SomeException (Response Fdata)
+  print succeeding
