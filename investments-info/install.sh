87a0e8813b3ccb7519397358cb2851793d93108d
Sasa Bogicevic <bogicevicsasa@yahoo.com>
2017-08-29 16:08:54 +0200

    Add deploy and install script

---
+++ investments-info/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo chmod a+x investments-info
+sudo mv haskell-serbia /var/www/html/investments-info/investments-info/investments-info_bin
+sudo killall investments-info_bin
+sleep 3
+cd /var/www/html/investments-info/investments-info/
+sudo cp investments-info.sqlite3 /var/www/html/
+sudo git stash
+sudo git pull origin master
+sudo cp /var/www/html/investments-info.sqlite3 /var/www/html/investments-info/investments-info/
+nohup sudo ./investments-info_bin > /dev/null  < /dev/null &
