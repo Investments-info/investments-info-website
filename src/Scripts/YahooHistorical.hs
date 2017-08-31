5b23524c49ee923d43acaea5cdfe9371ef6786da
Sasa Bogicevic <bogicevicsasa@yahoo.com>
2017-08-31 17:49:01 +0200

    Add "cookie" url

--- src/Scripts/YahooHistorical.hs
+++ src/Scripts/YahooHistorical.hs
@@ -9,6 +9,10 @@ import qualified Data.ByteString.Char8 as S8
 import qualified Data.Yaml             as Yaml
 import           Network.HTTP.Simple
 
+-- this url is used to get the cookie
+cookie_url :: Request
+cookie_url = "https://finance.yahoo.com/quote/AAPL/history"
+
 api_url :: Request
 api_url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20%28%22SLV%22%29&env=store://datatables.org/alltableswithkeys"
 
