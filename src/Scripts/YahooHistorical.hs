33535f674c31294e693d39502c91cce08193d538
Sasa Bogicevic <bogicevicsasa@yahoo.com>
2017-09-01 16:36:45 +0200

    Add function to generate api url

--- src/Scripts/YahooHistorical.hs
+++ src/Scripts/YahooHistorical.hs
@@ -11,13 +11,24 @@ import           Network.HTTP.Simple
 import           Web.Cookie
 import Generics.Deriving
 
+intervals :: [Text]
+intervals = ["1d", "1wk", "1mo"]
 
 -- this url is used to get the cookie
 cookie_url :: Text -> Text
 cookie_url ticker =  "https://finance.yahoo.com/quote/" <> ticker <> "/history"
 
-api_url :: Request
-api_url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20%28%22SLV%22%29&env=store://datatables.org/alltableswithkeys"
+api_url :: Text -> Text -> Text -> Text -> Text -> Maybe Request
+api_url ticker startDate endDate interval crumb = parseRequest $ unpack $ "https://query1.finance.yahoo.com/v7/finance/download/"
+    <> ticker <>
+    "?period1="
+    <> startDate <>
+    "&period2="
+    <> endDate <>
+    "&interval="
+    <> interval <>
+    "&events=history&crumb="
+    <> crumb
 
 getYahooHistorical :: IO ()
 getYahooHistorical = do
@@ -26,10 +37,10 @@ getYahooHistorical = do
   let responseHeaders = getResponseHeaders response :: [(HeaderName, ByteString)]
   let cookieDict = lookup "set-cookie" responseHeaders
   case cookieDict of
-      Nothing -> print "No set-cookie key found"
+      Nothing -> print ("No set-cookie key found" :: Text)
       Just a -> do
           let cookiesList =  parseCookies a
               ycookie = lookup "B" cookiesList
           case ycookie of
-              Nothing -> print "No B key found"
+              Nothing -> print ("No B key found" :: Text)
               Just c -> print c
