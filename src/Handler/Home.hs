7220e7acb9aa9517e004bc08ef5fa7f9b33aec62
Sasa Bogicevic <bogicevicsasa@yahoo.com>
2017-09-08 23:25:31 +0200

    Get all to compile and now just do fetch and insert logic

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -3,12 +3,15 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleContexts #-}
 module Handler.Home where
 
 import Import
 import Yesod.Form.Bootstrap3 (BootstrapFormLayout (..), renderBootstrap3)
 import Text.Julius (RawJS (..))
 import qualified Text.HTML.Fscraper as F
+import Database.Esqueleto as E
+import Database.Esqueleto.Internal.Language
 
 -- Define our data that will be used for creating the form.
 data FileForm = FileForm
@@ -18,9 +21,8 @@ data FileForm = FileForm
 
 getHomeR :: Handler Html
 getHomeR = do
-  topn <- liftIO getTopStory
-  siden <- liftIO getSideStories
-  featuren <- liftIO getFeatureStories
+  -- firststory <- runDB $ selectFirst [StoryTitle Import.!=. storyTitle ""]
+  allStories <- runDB $ selectList [] [Desc StoryCreated]
   (formWidget, formEnctype) <- generateFormPost sampleForm
   let submission = Nothing :: Maybe FileForm
       handlerName = "getHomeR" :: Text
@@ -30,44 +32,33 @@ getHomeR = do
     setTitle "Finance portal"
     $(widgetFile "homepage")
 
-getTopStory :: IO [F.News]
+
+getTopStory :: IO [Handler (Key Story)]
 getTopStory = do
   headStory <- F.topStory "olympics-topStory" F.reutersUrl
   case headStory of
     Nothing -> return []
-    Just a -> return a
+    Just a -> do
+      return $ map insertStory $ a
 
-getFeatureStories :: IO [F.News]
+
+getFeatureStories :: IO [Handler (Key Story)]
 getFeatureStories = do
   stories <- F.featureNews "feature" F.reutersUrl
   case stories of
     Nothing -> return []
-    Just a -> return a
+    Just a -> do
+      return $ map insertStory $ a
 
-getSideStories :: IO [F.News]
+
+getSideStories :: IO [Handler (Key Story)]
 getSideStories = do
   stories <- F.leftColumnNews "column2" F.reutersUrl
   case stories of
     Nothing -> return []
-    Just a -> return a
+    Just a -> do
+      return $ map insertStory $ a
 
-
-postHomeR :: Handler Html
-postHomeR = do
-  topn <- liftIO getTopStory
-  siden <- liftIO getSideStories
-  featuren <- liftIO getFeatureStories
-  ((result, formWidget), formEnctype) <- runFormPost sampleForm
-  let handlerName = "postHomeR" :: Text
-      submission =
-        case result of
-          FormSuccess res -> Just res
-          _ -> Nothing
-  defaultLayout $ do
-    let (commentFormId, commentTextareaId, commentListId) = commentIds
-    aDomId <- newIdent
-    setTitle "Welcome To Yesod!"
-    $(widgetFile "homepage")
 
 sampleForm :: Form FileForm
 sampleForm =
@@ -86,8 +77,10 @@ sampleForm =
           [("class", "form-control"), ("placeholder", "File description")]
       }
 
+
 commentIds :: (Text, Text, Text)
 commentIds = ("js-commentForm", "js-createCommentTextarea", "js-commentList")
+
 
 insertStory :: F.News -> Handler (Key Story)
 insertStory news = do
@@ -101,3 +94,16 @@ insertStory news = do
 
   sid  <- runDB $ insert $ story
   return sid
+
+
+selectCount
+  :: (BaseBackend backend ~ SqlBackend,
+      Database.Esqueleto.Internal.Language.From
+        SqlQuery SqlExpr SqlBackend t,
+      MonadIO m, Num a, IsPersistBackend backend,
+      PersistQueryRead backend, PersistUniqueRead backend,
+      PersistField a) =>
+     (t -> SqlQuery a1) -> ReaderT backend m a
+selectCount q = do
+  res <- select $ from (\x -> q x >> return countRows)
+  return $ fromMaybe 0 $ (\(Value a) -> a) <$> headMay res
