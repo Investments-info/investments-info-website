03b772ca52fc1b1507fb0fc817fb71c6ee5e661f
Sasa Bogicevic <bogicevicsasa@yahoo.com>
2017-09-12 21:36:31 +0200

    Clean up code

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -7,72 +7,39 @@
 module Handler.Home where
 
 import Import
-import Yesod.Form.Bootstrap3 (BootstrapFormLayout (..), renderBootstrap3)
-import Text.Julius (RawJS (..))
 import qualified Text.HTML.Fscraper as F
-import Database.Esqueleto as E
-import Database.Esqueleto.Internal.Language
 import Data.Time.Clock (diffUTCTime)
 
--- Define our data that will be used for creating the form.
-data FileForm = FileForm
-  { fileInfo :: FileInfo
-  , fileDescription :: Text
-  }
 
 getHomeR :: Handler Html
 getHomeR = do
   now <- liftIO getCurrentTime
+  topnews <- liftIO getTopStory
+  fnews <- liftIO getFeatureStories
+  snews <- liftIO getSideStories
+  let topstories =  mapM convertImageStory topnews now
+  let fstories =  mapM convertImageStory fnews now
+  let sstories =  mapM convertStory snews now
   firststory <- runDB $ selectFirst [] [Desc StoryCreated]
   case firststory of
       Nothing -> do
-              topnews <- liftIO getTopStory
-              fnews <- liftIO getFeatureStories
-              snews <- liftIO getSideStories
-              let topstories =  mapM convertImageStory topnews now
-              let fstories =  mapM convertImageStory fnews now
-              let sstories =  mapM convertStory snews now
-              mapM (runDB . insert) (topstories <> fstories <> sstories)
+              _ <- mapM (runDB . insert) (topstories <> fstories <> sstories)
               allStories <- runDB $ selectList [] [Desc StoryCreated]
-              (formWidget, formEnctype) <- generateFormPost sampleForm
-              let submission = Nothing :: Maybe FileForm
-                  handlerName = "getHomeR" :: Text
               defaultLayout $ do
-                let (commentFormId, commentTextareaId, commentListId) = commentIds
-                aDomId <- newIdent
                 setTitle "Finance portal"
                 $(widgetFile "homepage")
       Just fs -> do
           let tdiff = diffUTCTime now  (storyCreated $ entityVal fs)
           if(tdiff > 3600) then
-             do
-              topnews <- liftIO getTopStory
-              fnews <- liftIO getFeatureStories
-              snews <- liftIO getSideStories
-              let topstories =  mapM convertImageStory topnews now
-              let fstories =  mapM convertImageStory fnews now
-              let sstories =  mapM convertStory snews now
-              mapM (runDB . insert) (topstories <> fstories <> sstories)
-              allStories <- runDB $ selectList [] [Desc StoryCreated]
-              (formWidget, formEnctype) <- generateFormPost sampleForm
-              let submission = Nothing :: Maybe FileForm
-                  handlerName = "getHomeR" :: Text
-              defaultLayout $ do
-                let (commentFormId, commentTextareaId, commentListId) = commentIds
-                aDomId <- newIdent
-                setTitle "Finance portal"
-                $(widgetFile "homepage")
+              do
+                _ <- mapM (runDB . insert) (topstories <> fstories <> sstories)
+                return ()
           else
-            do
-             allStories <- runDB $ selectList [] [Desc StoryCreated]
-             (formWidget, formEnctype) <- generateFormPost sampleForm
-             let submission = Nothing :: Maybe FileForm
-                 handlerName = "getHomeR" :: Text
-             defaultLayout $ do
-                let (commentFormId, commentTextareaId, commentListId) = commentIds
-                aDomId <- newIdent
-                setTitle "Finance portal"
-                $(widgetFile "homepage")
+              return ()
+          allStories <- runDB $ selectList [] [Desc StoryCreated]
+          defaultLayout $ do
+            setTitle "Finance portal"
+            $(widgetFile "homepage")
 
 
 getTopStory :: IO [F.News]
@@ -81,7 +48,6 @@ getTopStory = do
   case headStory of
     Nothing -> return []
     Just a -> return a
-
 
 
 getFeatureStories :: IO [F.News]
@@ -99,29 +65,6 @@ getSideStories = do
     Nothing -> return []
     Just a -> return a
 
-
-sampleForm :: Form FileForm
-sampleForm =
-  renderBootstrap3 BootstrapBasicForm $
-  FileForm <$> fileAFormReq "Choose a file" <*>
-  areq textField textSettings Nothing
-    -- Add attributes like the placeholder and CSS classes.
-  where
-    textSettings =
-      FieldSettings
-      { fsLabel = "What's on the file?"
-      , fsTooltip = Nothing
-      , fsId = Nothing
-      , fsName = Nothing
-      , fsAttrs =
-          [("class", "form-control"), ("placeholder", "File description")]
-      }
-
-
-commentIds :: (Text, Text, Text)
-commentIds = ("js-commentForm", "js-createCommentTextarea", "js-commentList")
-
-
 convertImageStory :: F.News -> UTCTime -> Story
 convertImageStory news now =  Story { storyTitle = pack $ F.newstitle news
         , storyLink = pack $ F.newslink news
@@ -138,15 +81,3 @@ convertStory news now =  Story { storyTitle = pack $ F.newstitle news
         , storyImage = Nothing
         , storyCreated = now
         }
-
-selectCount
-  :: (BaseBackend backend ~ SqlBackend,
-      Database.Esqueleto.Internal.Language.From
-        SqlQuery SqlExpr SqlBackend t,
-      MonadIO m, Num a, IsPersistBackend backend,
-      PersistQueryRead backend, PersistUniqueRead backend,
-      PersistField a) =>
-     (t -> SqlQuery a1) -> ReaderT backend m a
-selectCount q = do
-  res <- select $ from (\x -> q x >> return countRows)
-  return $ fromMaybe 0 $ (\(Value a) -> a) <$> headMay res
