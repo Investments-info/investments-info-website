ce16586b1b26eb986681ee0300ee8378c4b766d6
Sasa Bogicevic <bogicevicsasa@yahoo.com>
2017-09-18 17:00:38 +0200

    Rewrite to use MonadIO

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -15,64 +15,62 @@ import Helper.Helper  as H
 getHomeR :: Handler Html
 getHomeR = do
   now <- liftIO getCurrentTime
-  topnews <- liftIO getTopStory
-  fnews <- liftIO getFeatureStories
-  snews <- liftIO getSideStories
-  let topstories =  mapM convertImageStory topnews now
-      fstories =  mapM convertImageStory fnews now
-      sstories =  mapM convertStory snews now
-      allS     = topstories <> fstories <> sstories
+  topnews <- getTopStory
+  fnews <- getFeatureStories
+  snews <- getSideStories
+  let topstories = mapM convertImageStory topnews now
+      fstories = mapM convertImageStory fnews now
+      sstories = mapM convertStory snews now
+      allS = topstories <> fstories <> sstories
   firststory <- runDB $ selectFirst [] [Desc StoryCreated]
   case firststory of
-      Nothing -> do
-              _ <- mapM checkStorySaved allS
-              allStories <- runDB $ selectList [] [Desc StoryCreated, LimitTo 5]
-              defaultLayout $ do
-                setTitle "Finance portal"
-                $(widgetFile "homepage")
-      Just fs -> do
-          let tdiff = diffUTCTime now  (storyCreated $ entityVal fs)
-          if(tdiff > 3600) then
-              do
-                -- _ <- runDB  $ H.truncateTables
-                _ <- mapM checkStorySaved allS
-                return ()
-          else
-              return ()
-          allStories <- runDB $ selectList [] [Desc StoryCreated, LimitTo 5]
-          defaultLayout $ do
-            setTitle "Finance portal"
-            $(widgetFile "homepage")
+    Nothing -> do
+      _ <- mapM checkStorySaved allS
+      allStories <- runDB $ selectList [] [Desc StoryCreated, LimitTo 5]
+      defaultLayout $ do
+        setTitle "Finance portal"
+        $(widgetFile "homepage")
+    Just fs -> do
+      let tdiff = diffUTCTime now (storyCreated $ entityVal fs)
+      if (tdiff > 3600)
+        then do
+          _ <- mapM checkStorySaved allS
+          return ()
+        else return ()
+      allStories <- runDB $ selectList [] [Desc StoryCreated, LimitTo 5]
+      defaultLayout $ do
+        setTitle "Finance portal"
+        $(widgetFile "homepage")
 
 
 checkStorySaved :: Story -> HandlerT App IO (Maybe (Entity Story))
 checkStorySaved story = do
-    insertedStory <- runDB $ selectFirst [StoryHashId ==. storyHashId story] []
-    case insertedStory of
-        Nothing -> do
-            _ <- runDB $ insert story
-            return Nothing
-        Just s -> return $ Just s
+  insertedStory <- runDB $ selectFirst [StoryHashId ==. storyHashId story] []
+  case insertedStory of
+    Nothing -> do
+      _ <- runDB $ insert story
+      return Nothing
+    Just s -> return $ Just s
 
-getTopStory :: IO [F.News]
+getTopStory :: MonadIO m => m [F.News]
 getTopStory = do
-  headStory <- F.topStory "olympics-topStory" F.reutersUrl
+  headStory <- liftIO $ F.topStory "olympics-topStory" F.reutersUrl
   case headStory of
     Nothing -> return []
     Just a -> return a
 
 
-getFeatureStories :: IO [F.News]
+getFeatureStories :: MonadIO m => m [F.News]
 getFeatureStories = do
-  stories <- F.featureNews "feature" F.reutersUrl
+  stories <- liftIO $ F.featureNews "feature" F.reutersUrl
   case stories of
     Nothing -> return []
     Just a -> return a
 
 
-getSideStories :: IO [F.News]
+getSideStories :: MonadIO m => m [F.News]
 getSideStories = do
-  stories <- F.leftColumnNews "column2" F.reutersUrl
+  stories <- liftIO $ F.leftColumnNews "column2" F.reutersUrl
   case stories of
     Nothing -> return []
     Just a -> return a
