4a1b462a7f8a18c4cc4c934e58712c0248c83b4f
Sasa Bogicevic <bogicevicsasa@yahoo.com>
2017-09-28 00:54:43 +0200

    Start to work on parsing csv to adt

--- src/Helper/Helper.hs
+++ src/Helper/Helper.hs
@@ -8,31 +8,37 @@
 module Helper.Helper where
 
 import Database.Persist.Sql  (SqlBackend, rawSql, unSingle)
-import Data.Text
+import Data.Text (Text)
 import Data.Hashable
 import Import
 import LibYahoo (getYahooData)
+import qualified Data.CSV.Conduit as C hiding (CSV)
+import Data.CSV.Conduit.Conversion
+import Control.Monad (mzero)
+import qualified Data.Text.Encoding as T
+import qualified Data.ByteString.Lazy.UTF8 as DBLU
+import qualified Data.Vector as V
 
-data YahooData = YahooData
-  { yahooDataDate :: UTCTime
-  , yahooDataOpen :: Float
-  , yahooDataHigh :: Float
-  , yahooDataLow :: Float
-  , yahooDataClose :: Float
-  , yahooDataAdjClose :: Float
-  , yahooDataVolume :: Int
-  } deriving (Show, Eq)
+-- data YahooData = YahooData
+--   { yahooDataDate :: UTCTime
+--   , yahooDataOpen :: Float
+--   , yahooDataHigh :: Float
+--   , yahooDataLow :: Float
+--   , yahooDataClose :: Float
+--   , yahooDataAdjClose :: Float
+--   , yahooDataVolume :: Int
+--   } deriving (Show, Eq)
 
-data YD a where
-    YahooD :: YD YahooData
+-- data YD a where
+--     YahooD :: YD YahooData
 
 companyCodes :: [String]
 companyCodes = ["KO", "AAPL"]
 
 truncateTables :: MonadIO m => ReaderT SqlBackend m [Text]
 truncateTables = do
-    result <- rawSql "DELETE FROM 'story';" []
-    return (fmap unSingle result)
+  result <- rawSql "DELETE FROM 'story';" []
+  return (fmap unSingle result)
 
 makeHash :: Hashable a => a -> Int
 makeHash s = hash s
@@ -41,3 +47,44 @@ fetchYahooHistoricalData :: String -> HandlerT App IO ()
 fetchYahooHistoricalData companyCode = do
   graphData <- liftIO (getYahooData companyCode)
   return ()
+
+
+
+ ------------------------------------------------------------------------------------------------------
+
+data YahooData = YahooData
+  { yahooDataDate :: !Text
+  , yahooDataOpen :: !Text
+  , yahooDataHigh :: !Text
+  , yahooDataLow :: !Text
+  , yahooDataClose :: !Text
+  , yahooDataAdjClose :: !Text
+  , yahooDataVolume :: !Text
+  } deriving (Show, Eq)
+
+instance FromRecord YahooData where
+  parseRecord v
+    | length v == 7 =
+      YahooData <$> v .! 0 <*> v .! 1 <*> v .! 2 <*> v .! 3 <*> v .! 4 <*>
+      v .! 5 <*>
+      v .! 6
+    | otherwise = mzero
+
+instance ToRecord YahooData where
+  toRecord (YahooData yahooDataDate yahooDataOpen yahooDataHigh yahooDataLow yahooDataClose yahooDataAdjClose yahooDataVolume) =
+    record
+      [ toField yahooDataDate
+      , toField yahooDataOpen
+      , toField yahooDataHigh
+      , toField yahooDataLow
+      , toField yahooDataClose
+      , toField yahooDataAdjClose
+      , toField yahooDataVolume
+      ]
+
+readToType = do
+  yd <- getYahooData "AAPL"
+  let dataList = lines $ DBLU.toString yd
+  let vector = fromList dataList
+  let yList = fmap pack vector
+  return undefined -- $ fmap parseRecord yList
