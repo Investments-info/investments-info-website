b1eb1f0e5bd89ba7f69f4556c72b5351e7cdd61e
Sasa Bogicevic <bogicevicsasa@yahoo.com>
2017-09-29 10:59:54 +0200

    Managed to parse to YahooData
    TODO: handle exceptions

--- src/Helper/Helper.hs
+++ src/Helper/Helper.hs
@@ -12,25 +12,20 @@ import Data.Text (Text)
 import Data.Hashable
 import Import
 import LibYahoo (getYahooData)
-import qualified Data.CSV.Conduit as C hiding (CSV)
+import qualified Data.CSV.Conduit as CC hiding (CSV)
 import Data.CSV.Conduit.Conversion
 import Control.Monad (mzero)
 import qualified Data.Text.Encoding as T
-import qualified Data.ByteString.Lazy.UTF8 as DBLU
+import           Data.Time.Clock (UTCTime)
+import           Data.Time.Format
 import qualified Data.Vector as V
-
--- data YahooData = YahooData
---   { yahooDataDate :: UTCTime
---   , yahooDataOpen :: Float
---   , yahooDataHigh :: Float
---   , yahooDataLow :: Float
---   , yahooDataClose :: Float
---   , yahooDataAdjClose :: Float
---   , yahooDataVolume :: Int
---   } deriving (Show, Eq)
-
--- data YD a where
---     YahooD :: YD YahooData
+import qualified Data.ByteString               as B
+import qualified Data.ByteString.Internal      as BI
+import qualified Data.ByteString.Lazy          as BL
+import qualified Data.ByteString.Lazy.Internal as BLI
+import qualified Data.ByteString.Lazy.Char8 as C
+import Control.Monad.Trans.Class
+import Control.Monad.Trans.Maybe
 
 companyCodes :: [String]
 companyCodes = ["KO", "AAPL"]
@@ -51,6 +46,19 @@ fetchYahooHistoricalData companyCode = do
 
 
  ------------------------------------------------------------------------------------------------------
+
+-- data YahooData = YahooData
+--   { yahooDataDate :: UTCTime
+--   , yahooDataOpen :: Float
+--   , yahooDataHigh :: Float
+--   , yahooDataLow :: Float
+--   , yahooDataClose :: Float
+--   , yahooDataAdjClose :: Float
+--   , yahooDataVolume :: Int
+--   } deriving (Show, Eq)
+
+-- data YD a where
+--     YahooD :: YD YahooData
 
 data YahooData = YahooData
   { yahooDataDate :: !Text
@@ -73,7 +81,7 @@ instance FromRecord YahooData where
 instance ToRecord YahooData where
   toRecord (YahooData yahooDataDate yahooDataOpen yahooDataHigh yahooDataLow yahooDataClose yahooDataAdjClose yahooDataVolume) =
     record
-      [ toField yahooDataDate
+      [ toField yahooDataDate -- toField $ parseTimestamp "%d.%m.%Y %H:%M" yahooDataDate
       , toField yahooDataOpen
       , toField yahooDataHigh
       , toField yahooDataLow
@@ -82,9 +90,27 @@ instance ToRecord YahooData where
       , toField yahooDataVolume
       ]
 
+readToType :: IO (Parser YahooData)
 readToType = do
-  yd <- getYahooData "AAPL"
-  let dataList = lines $ DBLU.toString yd
-  let vector = fromList dataList
-  let yList = fmap pack vector
-  return undefined -- $ fmap parseRecord yList
+  yd <- liftIO $ getYahooData "AAPL"
+  let dataList = lines $ C.unpack yd
+  let cList = fmap C.pack dataList
+  let bList = fmap toStrict1 cList
+  let records = record bList
+  return $ parseRecord records
+
+printYlist = do
+    pl <- readToType
+    case runParser pl of
+        Left e -> print e
+        Right r -> print r
+
+toStrict1 :: C.ByteString -> B.ByteString
+toStrict1 = B.concat . C.toChunks
+
+
+parseTimestamp :: (Monad m, ParseTime t)
+               => String   -- ^ Format string
+               -> String   -- ^ Input string
+               -> m t
+parseTimestamp = parseTimeM True defaultTimeLocale
