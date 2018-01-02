0d6961c8b441992fdeaa75a1891e3658c16af1aa
Sasa Bogicevic <bogicevicsasa@yahoo.com>
2018-01-02 23:57:32 +0100

    [master] Create unsubscribe option

--- src/Handler/Profile.hs
+++ src/Handler/Profile.hs
@@ -1,36 +1,65 @@
+{-# LANGUAGE RecordWildCards #-}
 module Handler.Profile where
 
 import Import
+import MailchimpSimple as MC
+import Data.Maybe (fromJust)
+
+-- move this to some helper
+listName :: String
+listName = "investments-info"
+
+apiKey :: String
+apiKey = "ab4685034f82cdd3c97286e4839b7cee-us17"
 
 getProfileR :: Handler Html
 getProfileR = do
-    -- redirectIfNotLoggedIn HomeR
-    (profileFormWidget, _) <- generateFormPost profileForm
-    renderProfile profileFormWidget
+    redirectIfNotLoggedIn HomeR
+    user <- getUser
+    case user of
+        Just (Entity _ u) -> do
+            (profileFormWidget, _) <- generateFormPost $ profileForm u
+            renderProfile u profileFormWidget
+        Nothing -> do
+            setMessage "You must be logged-in to see this page!"
+            return $ toHtml ("Nothing" :: Text)
 
 postProfileR :: Handler Html
 postProfileR = do
   redirectIfNotLoggedIn HomeR
-  ((result, widget), _) <- runFormPost profileForm
-  case result of
-    FormSuccess email -> error "implement post!"
-    _ -> renderProfile widget
-profileForm :: Form Bool
-profileForm =
-  renderDivs $
-      areq checkBoxField checkboxSettings (Just True)
+  newsletter <- lookupPostParam "newsletter"
+  email <- lookupPostParam "email"
+  Just (Entity dbUKey _) <- getUser
+  case newsletter of
+      Just "yes" -> do
+            _ <- liftIO $ MC.addSubscriber apiKey listName (unpack $ fromJust email) "newsletter-user" "subscribed"
+            _ <- runDB $ setUserForNewsletter (Just 1) dbUKey
+            getProfileR
+      Just _ -> do
+            _ <- liftIO $ MC.addSubscriber apiKey listName (unpack $ fromJust email) "newsletter-user" "subscribed"
+            _ <- runDB $ setUserForNewsletter Nothing dbUKey
+            getProfileR
+      Nothing -> do
+            _ <- liftIO $ MC.removeSubscriber apiKey (unpack $ fromJust email) listName
+            _ <- runDB $ setUserForNewsletter Nothing dbUKey
+            getProfileR
 
-renderProfile :: Widget -> Handler Html
-renderProfile widget = do
+profileForm :: User -> Form Bool
+profileForm User {..} =
+  renderDivs $
+      areq checkBoxField checkboxSettings (Just (isJust userNewsletter))
+
+renderProfile :: User -> Widget -> Handler Html
+renderProfile u widget = do
   baseLayout "Profile" Nothing [whamlet|
 
 <section id="content" class="main">
     <div>
       <div .col-md-6>
         <h3> User profile
-        <form method="POST" action="@{ProfileR}">
-        ^{widget}
-         <div .clearfix>
+        <form method="POST" action="@{ProfileR}" name="profile_post">
+          ^{widget}
+          <input type="hidden" value="#{userEmail u}" name="email" />
           <input .button type="submit" value="Submit">
      <div .clearfix>
 |]
