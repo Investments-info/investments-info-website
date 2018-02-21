2b7e6eff9edbdb6e50ed0a3daa0e8e08a65765b7
Sasa Bogicevic <bogicevicsasa@yahoo.com>
2018-02-21 18:24:01 +0100

    Change stylish haskell

--- news-fetcher/src/Text/HTML/Freader.hs
+++ news-fetcher/src/Text/HTML/Freader.hs
@@ -1,8 +1,9 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards   #-}
 
-
-module Text.HTML.Freader where
+module Text.HTML.Freader
+  ( parseDocument
+  ) where
 
 import           Control.Exception.Safe (Exception, SomeException (..), try)
 import           Control.Lens (set)
@@ -20,27 +21,29 @@ type RssM = ExceptT String IO
 data RssFeed = RssFeed
   { rssTitle :: Text
   , rssUrl   :: Text
-  } deriving Show
+  } deriving (Show)
 
-data RssException = RssException String deriving Show
+data RssException =
+  RssException String
+  deriving (Show)
 
 instance Exception RssException
 
 parseRss :: B.ByteString -> RssM Document
 parseRss bs = res
-    where
-      res =
-        case parseLBS def bs of
-          Left (SomeException a) -> throwError (show a)
-          Right d                -> return d
+  where
+    res =
+      case parseLBS def bs of
+        Left (SomeException a) -> throwError (show a)
+        Right d                -> return d
 
 getFeed :: Text -> RssM Document
 getFeed rssUrl = do
   manager <- lift $ newManager $ managerSetProxy noProxy tlsManagerSettings
   lift $ setGlobalManager manager
   rssRequest <- parseRequest $ unpack rssUrl
-  crumb <- lift $
-    try (httpLbs rssRequest manager) :: RssM (Either RssException (Response ByteString))
+  crumb <-
+    lift $ try (httpLbs rssRequest manager) :: RssM (Either RssException (Response ByteString))
   case fmap responseBody crumb of
     Left (RssException e) -> throwError e
     Right a               -> parseRss a
@@ -48,15 +51,25 @@ getFeed rssUrl = do
 -- rssLink = "http://feeds.reuters.com/reuters/businessNews"
 parseDocument :: Text -> IO (Maybe [RssFeed])
 parseDocument url = do
-    doc <- runExceptT (getFeed url)
-    case doc of
-      Left e -> return Nothing
-      Right d -> do
-        let cursor = fromDocument d
-        let titles = child cursor >>= element "channel" >>= child >>= element "item" >>= child >>= element "title" >>= descendant >>= content
-        let links = child cursor >>= element "channel" >>= child >>= element "item" >>= child >>= element "link" >>= descendant >>= content
-        let joinData = Prelude.zip titles links
-        return $ Just $ Prelude.map createFeed joinData
+  doc <- runExceptT (getFeed url)
+  case doc of
+    Left e -> return Nothing
+    Right d -> do
+      let cursor = fromDocument d
+      let titles =
+            child cursor >>= element "channel" >>= child >>= element "item" >>=
+            child >>=
+            element "title" >>=
+            descendant >>=
+            content
+      let links =
+            child cursor >>= element "channel" >>= child >>= element "item" >>=
+            child >>=
+            element "link" >>=
+            descendant >>=
+            content
+      let joinData = Prelude.zip titles links
+      return $ Just $ Prelude.map createFeed joinData
 
 createFeed :: (Text, Text) -> RssFeed
-createFeed l = RssFeed { rssTitle = fst l, rssUrl = snd l}
+createFeed l = RssFeed {rssTitle = fst l, rssUrl = snd l}
