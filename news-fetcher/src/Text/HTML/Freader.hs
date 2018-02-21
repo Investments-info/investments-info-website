2b8c307698c8929853cbe1038bbc9698896eed0b
Sasa Bogicevic <bogicevicsasa@yahoo.com>
2018-02-21 09:04:00 +0100

    Use custom monad, parse xml

--- news-fetcher/src/Text/HTML/Freader.hs
+++ news-fetcher/src/Text/HTML/Freader.hs
@@ -1,45 +1,69 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
+
 module Text.HTML.Freader where
 
 import           Control.Exception.Safe (Exception, SomeException (..), try)
-import           Data.ByteString.Lazy as B
+import           Control.Lens (set)
+import           Control.Monad.Except (ExceptT, lift, runExceptT, throwError)
+import           Data.ByteString.Lazy as B hiding (map, zip)
+import           Data.Monoid ((<>))
 import qualified Data.Text as T
-import           Data.Typeable (TypeRep, Typeable, typeRep)
 import           Network.HTTP.Client
 import           Network.HTTP.Client.TLS
-import           Text.XML (Document, Element, def, documentRoot, parseLBS)
+import           Text.XML (Document, def, parseLBS)
+import           Text.XML.Cursor
 
 rssLink :: T.Text
 rssLink = "http://feeds.reuters.com/reuters/businessNews"
 
-data RssException = RssException String TypeRep
-    deriving (Typeable)
+type RssM = ExceptT String IO
 
-instance Show RssException where
-  show (RssException s typ) = Prelude.concat
-    [ "Unable to parse as "
-    , show typ
-    , ": "
-    , show s
-    ]
+data RssFeed = RssFeed
+  { rssTitle :: T.Text
+  , rssUrl   :: T.Text
+  } deriving Show
+
+data RssException = RssException String deriving Show
+
 
 instance Exception RssException
 
-parseRss :: B.ByteString -> Either RssException Document
+parseRss :: B.ByteString -> RssM Document
 parseRss bs = res
     where
       res =
         case parseLBS def bs of
-          Left (SomeException a) -> Left $ RssException (show a) (typeRep res)
-          Right d                -> Right d
+          Left (SomeException a) -> throwError (show a)
+          Right d                -> return d
 
-getFeed :: T.Text -> IO (Either RssException Element)
+getFeed :: T.Text -> RssM Document
 getFeed rssUrl = do
-  manager <- newManager $ managerSetProxy noProxy tlsManagerSettings
-  setGlobalManager manager
+  manager <- lift $ newManager $ managerSetProxy noProxy tlsManagerSettings
+  lift $ setGlobalManager manager
   rssRequest <- parseRequest $ T.unpack rssUrl
-  crumb <-
-    try (httpLbs rssRequest manager) :: IO (Either RssException (Response ByteString))
+  crumb <- lift $
+    try (httpLbs rssRequest manager) :: RssM (Either RssException (Response ByteString))
   case fmap responseBody crumb of
-    Left e  -> return $ Left $ RssException (show e) (typeRep (fmap responseBody crumb))
-    Right a -> return $ fmap documentRoot (parseRss a)
+    Left (RssException e) -> throwError e
+    Right a               -> parseRss a
+
+-- parseDocument :: IO [RssFeed]
+parseDocument = do
+    doc <- runExceptT (getFeed rssLink)
+    -- _ <- print doc
+    case doc of
+      Left e -> return $ [RssFeed {rssTitle = "", rssUrl = ""}]
+      Right d -> do
+        let cursor = fromDocument d
+        let titles = child cursor >>= element "channel" >>= child >>= element "item" >>= child >>= element "title" >>= descendant >>= content
+        let links = child cursor >>= element "channel" >>= child >>= element "item" >>= child >>= element "link" >>= descendant >>= content
+        -- let description = child cursor >>= element "channel" >>= child >>= element "item" >>= child >>= element "description" >>= descendant >>= content
+        -- _ <- print $ map ((set rssDescription) . (set rssUrl) . createFeedTitle) titles links description
+        let joinData = zip titles links
+        let f = map createFeed joinData
+        undefined -- $
+
+
+createFeed :: (T.Text, T.Text) -> RssFeed
+createFeed l = RssFeed { rssTitle = fst l, rssUrl = snd l}
