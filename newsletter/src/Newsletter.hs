26a5f7d184c191f8f34d196995e8b7a22e06d62b
Sasa Bogicevic <bogicevicsasa@yahoo.com>
2018-02-22 00:50:40 +0100

    Add newsletter module

---
+++ newsletter/src/Newsletter.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE LambdaCase        #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Newsletter where
+
+import           Network.SES (PublicKey (..), Region (USEast1), SESError, SESResult (..),
+                              SecretKey (..), sendEmailBlaze)
+
+import qualified Text.Blaze.Html5 as H
+import qualified Text.Blaze.Html5.Attributes as A
+
+-- aws-access-key:  AKIAIULHBINBL7QQ5F7Q
+-- aws-secret-key:  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
+
+main :: IO ()
+main = sendMail >>= \case
+         Error e -> putStrLn $ show e
+         Success -> putStrLn "Email sent successfully!"
+
+sendMail :: IO SESResult
+sendMail = sendEmailBlaze publicKey secretKey region from to subject html
+ where
+   publicKey = PublicKey "AKIAI6GDZ5ELIC7ABKJA"
+   secretKey = SecretKey "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"
+   region    = USEast1
+   from    = "contact@investments-info.com"
+   to      = ["contact@investments-info.com"]
+   subject = "Test Subject"
+   html = H.html $ do
+            H.body $ do
+               H.img H.! A.src "http://haskell-lang.org/static/img/logo.png"
+               H.h1 "Html email! Hooray"
