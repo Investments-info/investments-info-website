7ac04c6b4bdeab9b6e8a0809cfd1727e138629f1
Sasa Bogicevic <bogicevicsasa@yahoo.com>
2018-02-28 23:26:40 +0100

    Finish aws email verification

--- newsletter/src/II/Newsletter.hs
+++ newsletter/src/II/Newsletter.hs
@@ -1,16 +1,37 @@
-{-# LANGUAGE LambdaCase        #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module II.Newsletter
   ( sesEmail
+  , verifyEmail
   , News(..)
   ) where
 
-import           Data.ByteString (ByteString)
+import Control.Exception (SomeException, try)
+import Crypto.Hash (Digest, SHA256, hmac, hmacGetDigest)
+import Data.ByteString (ByteString)
+import Data.ByteString (ByteString)
+import Data.ByteString.Base64 (encode)
+import Data.ByteString.Char8 (concat, pack, unpack)
+import qualified Data.ByteString.Char8 as C8
 import qualified Data.ByteString.Lazy as L
-import           Data.Text (Text, pack)
-import           Network.SES (PublicKey (..), Region (USEast1), SESResult (..), SecretKey (..),
-                              sendEmailBlaze)
+import qualified Data.ByteString.Lazy as L
+import Data.Byteable (toBytes)
+import Data.Monoid ((<>))
+import Data.Text (Text, pack)
+import Data.Text.Lazy.Encoding (encodeUtf8)
+import qualified Data.Text as T
+import Data.Text.Lazy (fromStrict)
+import Data.Time.Clock (getCurrentTime)
+import Data.Time.Format (defaultTimeLocale, formatTime)
+import Network.Http.Client
+       (Connection, Method(POST), baselineContextSSL, buildRequest,
+        closeConnection, encodedFormBody, http, openConnectionSSL,
+        sendRequest, setContentType, setHeader)
+import Network.SES
+       (PublicKey(..), Region(USEast1), SESErrorType(..), SESResult(..),
+        SecretKey(..), sendEmailBlaze)
+import OpenSSL (withOpenSSL)
 import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as A
 
@@ -20,16 +41,18 @@ awsAccessKey = "AKIAI6GDZ5ELIC7ABKJA"
 awsSecretKey :: ByteString
 awsSecretKey = "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"
 
+type ConnectionError a = IO (Either SomeException a)
+
 data News = News
   { _nTitle :: Text
-  , _nLink  :: Text
+  , _nLink :: Text
   } deriving (Show)
 
 sesEmail :: [L.ByteString] -> [News] -> IO (Either Text Text)
 sesEmail to n =
   sendMail to n >>= \case
-    Error e -> return $ Left $ pack (show e)
-    Success -> return $ Right (pack "success")
+    Error e -> return $ Left $ T.pack (show e)
+    Success -> return $ Right (T.pack "success")
 
 sendMail :: [L.ByteString] -> [News] -> IO SESResult
 sendMail emailList n =
@@ -70,3 +93,66 @@ htmlIntercalate sep (x:xs) = do
   sep
   htmlIntercalate sep xs
 htmlIntercalate _ [] = mempty
+
+verifyEmail :: Text -> IO ByteString
+verifyEmail email = makeRequest publicKey secretKey region (encodeUtf8 (fromStrict email))
+  where
+    publicKey = PublicKey awsAccessKey
+    secretKey = SecretKey awsSecretKey
+    region = USEast1
+
+makeSig
+  :: ByteString -- ^ Payload
+  -> ByteString -- ^ Key
+  -> ByteString
+makeSig payload key =
+  encode $ toBytes (hmacGetDigest $ hmac key payload :: Digest SHA256)
+
+makeRequest
+  :: PublicKey -- ^ AWS Public Key
+  -> SecretKey -- ^ AWS Secret Key
+  -> Region -- ^ The Region to send the Request
+  -> L.ByteString -- ^ The Email to verify
+  -> IO ByteString
+makeRequest (PublicKey publicKey) (SecretKey secretKey) region verify =
+  withOpenSSL $ do
+    now <- getCurrentTime
+    let date = C8.pack $ format now
+        sig = makeSig date secretKey
+        format = formatTime defaultTimeLocale "%a, %e %b %Y %H:%M:%S %z"
+        auth =
+          C8.concat
+            [ "AWS3-HTTPS AWSAccessKeyId="
+            , publicKey
+            , ", Algorithm=HmacSHA256, Signature="
+            , sig
+            ]
+        queryString =
+          ("Action", "VerifyEmailIdentity") :
+          ("EmailAddress", L.toStrict verify) : []
+    req <-
+      buildRequest $ do
+        http POST "/"
+        setContentType "application/x-www-form-urlencoded"
+        setHeader "X-Amzn-Authorization" auth
+        setHeader "Date" date
+    ctx <- baselineContextSSL
+    let
+    connResult <-
+      try
+        (openConnectionSSL
+           ctx
+           ("email." <> C8.pack (show region) <> ".amazonaws.com")
+           443) :: ConnectionError Connection
+    case connResult of
+      Left s -> connectionError s
+      Right con -> do
+        result <-
+          try (sendRequest con req $ encodedFormBody queryString) :: ConnectionError ()
+        case result of
+          Left s -> connectionError s
+          Right _ -> do
+            closeConnection con
+            return "Success"
+  where
+    connectionError = return . C8.pack . show
--- src/Handler/NewsletterManager.hs
+++ src/Handler/NewsletterManager.hs
@@ -1,6 +1,8 @@
 {-# OPTIONS_GHC -Wno-unused-matches #-}
+
 module Handler.NewsletterManager where
 
+import II.Newsletter
 import Import
 import MailchimpSimple as MC
 
@@ -10,49 +12,67 @@ listName = "investments-info"
 apiKey :: String
 apiKey = "ab4685034f82cdd3c97286e4839b7cee-us17"
 
-
 getNewsletterManagerR :: Handler Html
 getNewsletterManagerR = do
-  -- redirectIfLoggedIn HomeR
   (signupFormWidget, _) <- generateFormPost signupForm
   renderSignup signupFormWidget
 
 postNewsletterManagerR :: Handler Html
 postNewsletterManagerR = do
-  -- redirectIfLoggedIn HomeR
   ((result, widget), _) <- runFormPost signupForm
   case result of
     FormSuccess email -> do
       maybeUser <- runDB (getUserForNewsletter email)
       case maybeUser of
         Nothing -> do
-            (Entity dbUserKey _) <- runDB $ createUserForNewsletter email "dummy-pass" (Just 1)
-            _ <- liftIO $ MC.addSubscriber apiKey listName (unpack email) "newsletter-user" "subscribed"
-            setUserSession dbUserKey True
-            setMessage "You have signed-up for our newsletter! Expect it in your inbox once a week !"
-            redirect ProfileR
+          (Entity dbUserKey _) <-
+            runDB $ createUserForNewsletter email "dummy-pass" (Just 1)
+          _ <-
+            liftIO $
+            MC.addSubscriber
+              apiKey
+              listName
+              (unpack email)
+              "newsletter-user"
+              "subscribed"
+          _ <- liftIO $ verifyEmail email
+          setUserSession dbUserKey True
+          setMessage
+            "You have signed-up for our newsletter! Expect it in your inbox once a week !"
+          redirect ProfileR
         Just (Entity dbUKey dbUser) -> do
-            case userNewsletter dbUser of
-                Just 1 -> do
-                    setMessage "You are already signed-up for our newsletter!"
-                    redirect HomeR
-                _ -> do
-                    dbUserKeyU <- runDB $ setUserForNewsletter (Just 1) dbUKey
-                    _ <- liftIO $ MC.addSubscriber apiKey listName (unpack email) "newsletter-user" "subscribed"
-                    setMessage "You have signed-up for our newsletter! Expect it in your inbox once a week !"
-                    setUserSession dbUserKeyU True
-                    redirect ProfileR
+          case userNewsletter dbUser of
+            Just 1 -> do
+              setMessage "You are already signed-up for our newsletter!"
+              redirect HomeR
+            _ -> do
+              dbUserKeyU <- runDB $ setUserForNewsletter (Just 1) dbUKey
+              _ <-
+                liftIO $
+                MC.addSubscriber
+                  apiKey
+                  listName
+                  (unpack email)
+                  "newsletter-user"
+                  "subscribed"
+              _ <- liftIO $ verifyEmail email
 
+              setMessage
+                "You have signed-up for our newsletter! Expect it in your inbox once a week !"
+              setUserSession dbUserKeyU True
+              redirect ProfileR
     _ -> renderSignup widget
 
 signupForm :: Form Text
 signupForm =
-  renderDivs $
-      areq textField (named "email" (placeheld "Email")) Nothing
+  renderDivs $ areq textField (named "email" (placeheld "Email")) Nothing
 
 renderSignup :: Widget -> Handler Html
 renderSignup widget = do
-  baseLayout "Signup" Nothing [whamlet|
+  baseLayout
+    "Signup"
+    Nothing
+    [whamlet|
 
 <section id="content" class="main">
     <div>
@@ -64,7 +84,9 @@ renderSignup widget = do
      <div .clearfix>
 |]
 
-redirectIfLoggedIn :: (RedirectUrl App r) => r -> Handler ()
+redirectIfLoggedIn
+  :: (RedirectUrl App r)
+  => r -> Handler ()
 redirectIfLoggedIn r = do
   maybeUser <- getUser
   case maybeUser of
