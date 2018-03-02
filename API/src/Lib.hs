9c9d94d45304d9787193b5cdd50d7bb77dc64f72
Sasa Bogicevic <bogicevicsasa@yahoo.com>
2018-03-02 23:56:42 +0100

    Add html deps

--- API/src/Lib.hs
+++ API/src/Lib.hs
@@ -1,16 +1,20 @@
-{-# LANGUAGE DataKinds       #-}
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE TypeOperators   #-}
-module Lib
-    ( startApp
-    , app
-    ) where
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE TypeOperators     #-}
 
-import Data.Aeson
-import Data.Aeson.TH
-import Network.Wai
-import Network.Wai.Handler.Warp
-import Servant
+module Lib
+  ( startApp
+  , app
+  ) where
+
+import           Data.Aeson
+import           Data.Aeson.TH
+import           Network.Wai
+import           Network.Wai.Handler.Warp
+import           Servant
+import           Servant.HTML.Blaze
+import           Text.Blaze.Html5 hiding (main)
 
 data User = User
   { userId        :: Int
@@ -20,7 +24,7 @@ data User = User
 
 $(deriveJSON defaultOptions ''User)
 
-type API = "users" :> Get '[JSON] [User]
+type API =  "/home" :> Get '[HTML] Html :<|> "users" :> Get '[JSON] [User]
 
 startApp :: IO ()
 startApp = run 8080 app
@@ -32,9 +36,9 @@ api :: Proxy API
 api = Proxy
 
 server :: Server API
-server = return users
-
-users :: [User]
-users = [ User 1 "Isaac" "Newton"
-        , User 2 "Albert" "Einstein"
-        ]
+server = home :<|> users
+  where
+    home :: Handler Html
+    home = return $ h1 "Investments Info API"
+    users :: Handler [User]
+    users = return [User 1 "Isaac" "Newton", User 2 "Albert" "Einstein"]
